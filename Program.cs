363b28611b78d17afe84fb3e443ef5b9115d782f
Dambibimbus <dambibimbus@gmail.com>
2026-04-11 17:32:22 +0200

    feat: add server and logger (placeholders)add text file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace MarekDamikDungeon
+﻿using TcpServer2;
+
+namespace MarekDamikDungeon
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Server server = new Server(5000);
         }
     }
 }
